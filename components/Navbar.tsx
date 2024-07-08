8dc23116fcbb0e190d4c6cbc95f7b21f05107ed4
Plurmp <59399112+Plurmp@users.noreply.github.com>
2024-07-08 14:24:39 -0500

    added new branding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import '@aws-amplify/ui-react/styles.css';
 import ProfileDropdown from './ProfileDropdown';
 import { Amplify } from 'aws-amplify';
 import outputs from '@/amplify_outputs.json';
+import Image from 'next/image';
 
 Amplify.configure(outputs, { ssr: true });
 
@@ -14,8 +15,16 @@ const Navbar = ({children}: Readonly<{children: React.ReactNode}>) => {
         <header className='w-full bg-gradient-to-r from-blue-600 to-blue-400 border-b-2'>
             <nav className='flex justify-between items-center px-10 py-4'>
                 <Link href='/' className='flex items-center gap-2 text-white' title='Go Home'>
-                    <h1 className='text-xl font-bold'>REIT Jobs</h1>
-                    <p className='text-xs font-extralight italic'>All the <br/> REIT Jobs</p>
+                    <div className='flex rounded-sm bg-black h-10 w-10 p-[2px] justify-center'>
+                        <Image
+                            width={30}
+                            height={30}
+                            src="/img/all-the-reit-jobs.png"
+                            alt='AllTheREITJobs logo'
+                        />
+                    </div>
+                    <h1 className='text-xl font-bold'>AllTheREITJobs</h1>
+                    <p className='text-xs font-extralight italic'>Every REIT job<br/>in one place</p>
                 </Link>
                 <p className='text-md font-semibold text-red-700'>
                     THIS WEBSITE IS A WORK IN PROGRESS. JOBS ARE NOT BEING UPDATED LIVE.

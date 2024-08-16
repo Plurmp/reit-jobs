21f8c1cebe2be6cf745eb9134446d6dea6150e16
Plurmp <59399112+Plurmp@users.noreply.github.com>
2024-08-16 13:48:45 -0500

    added normalize page

---
+++ app/normalize/page.tsx
@@ -0,0 +1,84 @@
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import { runWithAmplifyServerContext } from "@/lib/amplifyServerUtils";
+import { cookies } from "next/headers";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth/server";
+import { generateClient } from "aws-amplify/api";
+import type { Schema } from "@/amplify/data/resource";
+import { normalizeLocation } from "@/lib/utils";
+
+Amplify.configure(outputs, { ssr: true });
+
+export default async function Normalize() {
+  let isAdmin = false;
+  let groups: string[] = [];
+
+  try {
+    groups = (await runWithAmplifyServerContext({
+      nextServerContext: { cookies },
+      operation: (contextSpec) =>
+        fetchAuthSession(contextSpec).then(
+          (output) => output.tokens?.accessToken.payload["cognito:groups"]
+        ),
+    })) as string[];
+    isAdmin = groups.includes("admin");
+  } catch (error) {
+    isAdmin = false;
+    groups = [];
+  }
+
+  async function normalizePositions(_: FormData) {
+    "use server";
+
+    Amplify.configure(outputs, { ssr: true });
+
+    try {
+      const groups = (await runWithAmplifyServerContext({
+        nextServerContext: { cookies },
+        operation: (contextSpec) =>
+          fetchAuthSession(contextSpec).then(
+            (output) => output.tokens?.accessToken.payload["cognito:groups"]
+          ),
+      })) as string[];
+      const currentUser = await runWithAmplifyServerContext({
+        nextServerContext: { cookies },
+        operation: (contextSpec) => getCurrentUser(contextSpec),
+      });
+      console.log(groups);
+      console.log(currentUser.username);
+    } catch (e) {
+      console.log(`could not get current user`);
+    }
+    const client = generateClient<Schema>();
+    const { data } = await client.models.Positions.list();
+    let previousUrls = new Set<string>();
+    let filteredData: typeof data = []
+    data.forEach(async (position) => {
+      if (previousUrls.has(position.url)) {
+        await client.models.Positions.delete({ id: position.id });
+        return;
+      }
+      previousUrls.add(position.url);
+      filteredData.push(position);
+    })
+    filteredData.forEach(async (position) => {
+      if (position.location !== null) {
+        const newLocation = normalizeLocation(position.location);
+        await client.models.Positions.update({ id: position.id, location: newLocation })
+      }
+    })
+  }
+  return (
+    <div className="flex flex-col rounded-md bg-white text-center items-center font-medium text-3xl m-4 p-4">
+      {!isAdmin ? (
+        <p>Access Denied</p>
+      ) : (
+        <form action={normalizePositions}>
+            <div className="border-purple-400 rounded-md bg-blue-500 p-2 text-white">
+              <button type="submit">Normalize Positions</button>
+            </div>
+        </form>
+      )}
+    </div>
+  )
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,8 @@
 
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import states from "states-us"
+import lookup from "country-code-lookup";
 
 interface ICompanyInfo {
   [key: string]: {
@@ -22,4 +24,34 @@ export function companyFullName(name: string | undefined): string | undefined {
   }
 
   return companyInfo[name].fullName;
+}
+
+export function normalizeLocation(locations?: string[]): string[] {
+  if (!locations) return [];
+
+  return locations.map((location) => {
+    location = location.trim();
+    states.forEach((state) => {
+      if (location.indexOf(state.name) === -1) {
+        return;
+      }
+      if (location.indexOf(state.name) === 0) {
+        location = state.abbreviation;
+        return;
+      }
+      location = location.replace(`, ${state.name}`, `, ${state.abbreviation}`);
+    })
+
+    const onlyCountry = lookup.byCountry(location)
+    if (onlyCountry !== null) {
+      location = onlyCountry.iso3;
+    }
+    const lastPart = location.match(/, ([a-zA-z ]+)$/)?.[1];
+    const lastCountry = !!lastPart ? lookup.byCountry(lastPart) : undefined;
+    if (!!lastCountry) {
+      location = location.replace(`, ${lastCountry.country}`, `, ${lastCountry.iso3}`);
+    }
+
+    return location
+  })
 }

3885ed05c5ebc150142c42a623a32faf03f4e5ec
Plurmp <plurmpmcflurnt@gmail.com>
2024-09-19 15:45:57 -0500

    more debugging, return full nextUrl object

--- app/api/route.ts
+++ app/api/route.ts
@@ -50,6 +50,6 @@ export async function GET(request: NextRequest) {
   return Response.json({
     positions: positions,
     nextToken: newNextToken,
-    recievedNextToken: nextToken,
+    recievedNextToken: request.nextUrl
   });
 }
